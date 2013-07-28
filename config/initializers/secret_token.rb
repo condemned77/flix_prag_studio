06348d077e5637935a0952da51fbaf37e239b440
Condemned77 <Jaguar@j.localdomain>
2013-07-28 19:33:29 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flix::Application.config.secret_key_base = 'a04c6b88b5d55af3cae2e2d577106a490b2a765a5beb531453aeba28b9d40284632ae20d7b620e9ae494be421f457b1f4daa82a7827bede81bb357c32590c71e'
